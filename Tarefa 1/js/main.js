70ef18dc1110854f005939adc6068dfea6f65233
lucassf <lucassoares-2010@hotmail.com>
2017-10-01 00:17:10 -0300

    ehisto

---
+++ Tarefa 1/js/main.js
@@ -0,0 +1,24 @@
+﻿function showPattern() {
+    var top_position = 100, left_position = 25;
+    var width = 300, height = 300;
+    var color_list = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+    var the_body = document.getElementById("theBody");
+
+    while (width > 50) {
+        var random_color = Math.floor(Math.random() * 7);
+        var this_div = document.createElement("div");
+
+        this_div.style.top = top_position + "px";
+        this_div.style.left = left_position + "px";
+        this_div.style.width = width + "px";
+        this_div.style.height = height + "px";
+        this_div.style.background = color_list[random_color];
+
+        the_body.appendChild(this_div);
+
+        top_position += 10;
+        left_position += 10;
+        width -= 20;
+        height -= 20;
+    }
+}
---
+++ Tarefa 2/js/worker.js
@@ -0,0 +1,10 @@
+﻿var i = 60;
+
+function timedCount() {
+    i = i - 1;
+    if (i < 0) i = 0;
+    postMessage(i);
+    setTimeout("timedCount()", 1000);
+}
+
+timedCount();
